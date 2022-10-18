bb4aa38b22e8705d65606c8d428f3afdbb17bbcd
tsuzat <aloksingh772002@gmail.com>
2022-10-18 19:26:58 +0530

    formatting_sync -> format : new vim.buf.lsp rule

--- nvim/after/ftplugin/dart.lua
+++ nvim/after/ftplugin/dart.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("Format", { clear = true }),
-  callback = function() vim.lsp.buf.formatting_sync() end
+  callback = function() vim.lsp.buf.format() end
 })
