af91327827adc3e7b8b92d8d17b28855c8a5f855
tsuzat <aloksingh772002@gmail.com>
2023-11-11 22:18:50 +0530

    use auto theme in lualine

--- nvim/lua/tsuzat/ui/lualine.lua
+++ nvim/lua/tsuzat/ui/lualine.lua
@@ -65,7 +65,7 @@ local lsp = {
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'NeoSolarized',
+    theme = 'auto',
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
     disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
