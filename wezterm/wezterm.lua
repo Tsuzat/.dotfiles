d056485b1bb5e2cab7b496a6a17cce3c51d40b3c
tsuzat <aloksingh772002@gmail.com>
2023-11-11 10:21:40 +0530

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require "wezterm"
 
+
 return {
   default_prog = {
     'C:\\Users\\aloks\\AppData\\Local\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe' },
@@ -7,30 +8,14 @@ return {
   font_size = 14,
   hide_tab_bar_if_only_one_tab = true,
   tab_bar_at_bottom = true,
-  window_background_opacity = 0.75,
+  window_background_opacity = 0.8,
   initial_rows = 30,
   initial_cols = 100,
-  colors = {
-    background = '#001217',
-    foreground = "#708183",
-    cursor_bg = "#708183",
-    selection_bg = "#fcf4dc",
-    selection_fg = "#001e26",
-    tab_bar = {
-      background = '#073642',
-      active_tab = {
-        fg_color = "#181926",
-        bg_color = "#268bd2",
-      },
-      inactive_tab = {
-        fg_color = "#CAD3F5",
-        bg_color = "#586e75",
-      },
-    },
-  },
+  color_scheme = 'NeoSolarized Dark',
+  color_scheme_dirs = { "~/.dotfiles/wezterm/colors" },
   window_padding = {
-    left = 0,
-    right = 0,
+    left = 2,
+    right = 2,
     top = 0,
     bottom = 0,
   },
