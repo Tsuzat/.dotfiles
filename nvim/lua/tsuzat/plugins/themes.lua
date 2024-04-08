6f2eacf09669746d4a13e73ab2f2f03e9d30a4c8
Tsuzat <dev.tsuzat@gmail.com>
2024-04-08 17:47:54 +0530

    add: catppuccin theme

--- nvim/lua/tsuzat/plugins/themes.lua
+++ nvim/lua/tsuzat/plugins/themes.lua
@@ -1,8 +1,20 @@
 return {
-  "Tsuzat/NeoSolarized.nvim",
-  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    vim.cmd.colorscheme "NeoSolarized"
-  end
+  {
+    dir = "~/Projects/NeoSolarized.nvim",
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd.colorscheme "NeoSolarized"
+    end
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true
+      })
+    end
+  }
 }
