e3971c9e7ab515a43b4f02da3268de42285d20bf
Tsuzat <dev.tsuzat@gmail.com>
2025-04-20 13:40:20 +0530

    refactor: fish shell config for new MacOS

--- fish/config.fish
+++ fish/config.fish
@@ -4,16 +4,6 @@ set fish_greeting
 set VIRTUAL_ENV_DISABLE_PROMPT "1"
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
-## Export variable need for qt-theme
-if type "qtile" >> /dev/null 2>&1
-   set -x QT_QPA_PLATFORMTHEME "qt5ct"
-end
-
-# Set settings for https://github.com/franciscolourenco/done
-set -U __done_min_cmd_duration 10000
-set -U __done_notification_urgency_level low
-
-
 ## Environment setup
 # Apply .profile: use this to put fish compatible .profile stuff in
 if test -f ~/.fish_profile
@@ -26,23 +16,6 @@ if test -d ~/.local/bin
         set -p PATH ~/.local/bin
     end
 end
-
-# Add depot_tools to PATH
-if test -d ~/Applications/depot_tools
-    if not contains -- ~/Applications/depot_tools $PATH
-        set -p PATH ~/Applications/depot_tools
-    end
-end
-
-
-## Starship prompt
-if status --is-interactive
-   source ("/usr/bin/starship" init fish --print-full-init | psub)
-end
-
-
-## Advanced command-not-found hook
-source /usr/share/doc/find-the-command/ftc.fish
 
 
 ## Functions
@@ -96,28 +69,19 @@ function copy
 end
 
 ## Useful aliases
-# Replace ls with exa
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
-alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first --icons'  # long format
-alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
-alias l.='exa -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+# Replace ls with eza
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.='eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
 alias ip='ip -color'
 
-# Replace some more things with better alternatives
-alias cat='bat --style header --style snip --style changes --style header'
-[ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
 
 # Common use
-alias grubup="sudo update-grub"
-alias fixpacman="sudo rm /var/lib/pacman/db.lck"
 alias tarnow='tar -acf '
 alias untar='tar -xvf '
 alias wget='wget -c '
-alias rmpkg="sudo pacman -Rdd"
-alias psmem='ps auxf | sort -nr -k 4'
-alias psmem10='ps auxf | sort -nr -k 4 | head -10'
-alias upd='/usr/bin/garuda-update'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
@@ -128,82 +92,24 @@ alias vdir='vdir --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='grep -F --color=auto'
 alias egrep='grep -E --color=auto'
-alias hw='hwinfo --short'                          # Hardware Info
-alias big="expac -H M '%m\t%n' | sort -h | nl"     # Sort installed packages according to size in MB
-alias gitpkg='pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
 
-# Get fastest mirrors
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
-alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
-alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
-alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
-
-# Help people new to Arch
-alias apt='man pacman'
-alias apt-get='man pacman'
-alias please='sudo'
-alias tb='nc termbin.com 9999'
-
-# Cleanup orphaned packages
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
-
-# Get the error messages from journalctl
-alias jctl="journalctl -p 3 -xb"
-
-# Recent installed packages
-alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
-
-
-## Run fastfetch if session is interactive
-if status --is-interactive && type -q fastfetch
-   fastfetch --load-config dr460nized
-end
-
-##### ALL MY PERSONALIZATION #####
-
-# Proxy Setting
-# export http_proxy=http://10.32.0.1:8080
-# export https_proxy=$http_proxy
-# export ftp_proxy=$http_proxy
-# export rsync_proxy=$http_proxy
-# export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com,::1"
-
-# Flutter Settings
-if test -d ~/flutter/bin
-    if not contains -- ~/flutter/bin $PATH
-        set -p PATH ~/flutter/bin
-    end
-end
-export CHROME_EXECUTABLE="/usr/bin/google-chrome-stable"
-
-# Custom Alias
-alias nv="nvim"
-alias cls="clear"
 alias sde="sudo -E"
-alias updateAll="sudo -E pacman -Syu && sudo -E pkgfile --update"
-alias updateGaruda="sudo -E garuda-update && sudo -E pkgfile --update"
-alias inst="sudo -E pacman -S"
-alias uninst="sudo -E pacman -Rns"
 alias del="rm -rf"
 alias defenstrate="sudo rm -rf"
 alias lg="lazygit"
 alias gi="git init"
 alias g="git"
-alias cpgitpat="xclip -selection clipboard < ~/.gitpat"
 alias :q="exit"
+alias cls="clear"
+alias v="nvim"
 
-# Custom function
-function pyProject
-    set count (count $argv | tr -d \n)
-    if test "$count" = 2
-        mkdir $argv[1] && cd $argv[1]
-        python -m venv $argv[2] && source ./$argv[2]/bin/activate.fish
-    else
-        echo "Wrong parameters"
-        echo "--> pyProject dir_name env_name"
-    end
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+## Starship prompt
+if status --is-interactive
+   source ("/opt/homebrew/bin/starship" init fish --print-full-init | psub)
 end
 
-function pyEnv
-    python -m venv $argv[1] && source ./$argv[1]/bin/activate.fish
-end
